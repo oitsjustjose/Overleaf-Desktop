41319ef4c85203528183fe99a0249e8c2d19cde4
oitsjustjose <stovallj1995@gmail.com>
2019-05-08 08:57:17 -0400

    Update buildscript

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
-echo "Buildling Gmail for Windows"
+echo "Buildling for Windows"
 
-electron-packager . "Overleaf Desktop" --overwrite --asar=true --platform=win32 --arch=ia32 --icon=assets/icons/win/overleaf.ico --prune=true --out=release-builds --version-string.CompanyName=oitsjustjose --version-string.FileDescription="A simple Gmail electron wrapper" --version-string.ProductName="Overleaf Desktop"
+electron-builder . --win
 
-echo "Building Gmail for macOS"
+echo "Building for macOS"
 
-electron-packager . --overwrite --platform=darwin --arch=x64 --icon=./assets/icons/macOS/overleaf.icns --prune=true --out=release-builds
+electron-builder .
 
-echo "Building Gmail for Linux"
+echo "Building for Linux"
 
-electron-packager . "Overleaf Desktop" --overwrite --asar=true --platform=linux --arch=x64 --icon=assets/icons/png/overleaf.png --prune=true --out=release-builds
+electron-builder . --linux
+
+electron-builder . --linux snap
